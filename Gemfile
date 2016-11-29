11a2b0fe98152835ac3d0225e03e35497ccecda2
Veronica Sonzini <mavesonzini@gmail.com>
2016-11-29 14:45:41 +0000

    feature test for creating a link

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+
+gem 'nokogiri'
+gem 'rack', '~>1.1'
+gem 'rspec', :require => 'spec'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'capybara'
+gem 'rspec-sinatra'
+gem 'sinatra'
---
+++ spec/features/viewing_links_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature 'see a list of links in the homepage' do
+
+  scenario 'view existing links' do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit '/links'
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content('Makers Academy')
+    end
+  end
+
+end
