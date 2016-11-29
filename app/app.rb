143052ca982e48165d9a68fd19aca06d13fa39e9
ajbater <amanda22@hotmail.co.uk>
2016-11-29 15:56:42 +0000

    passed first feature test

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
+require_relative 'models/link'
 
 class Bookmark_manager < Sinatra::Base
   get '/' do
-    
+
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
 
   # start the server if ruby file executed directly
